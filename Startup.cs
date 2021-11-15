1943f087c4f1d83449b840d2f5ee24df7eace1b2
Jason Mandabrandja <jasonmandabrandja@gmail.com>
2021-11-15 15:33:28 +0000

    fix ignore

--- Startup.cs
+++ Startup.cs
@@ -67,6 +67,7 @@ namespace ProduitMicroServices
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
